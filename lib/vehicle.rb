0ea2dae7d7e8887bf283c6fcb159a71e724690b0
Joanne Qiong Sun <joannesun44@gmail.com>
2020-03-18 16:40:56 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,9 +1,19 @@
 class Vehicle
   
-  attr_accessor :
+  attr_accessor :wheel_size, :wheel_number
   
   def initialie(wheel_size, wheel_number)
     @wheel_size = wheel_size
     @wheel_number = wheel_number
+  end 
+  
+  def go 
+    returns "vr"
+  end 
+  
+  def 
+
+
+
 
 end
